a3fbd2728ff1afacb2948fdb62ce6baf64516be1
Bobby M3 <2367573@gmail.com>
2020-04-25 13:13:56 +0300

    Поэкспериментировли со списками

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -14,14 +18,29 @@ namespace Snake
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
+            List<int> NumList = new List<int>();
+            NumList.Add(0);
+            NumList.Add(1);
+            NumList.Add(2);
+
+            int x = NumList[0];
+            int y = NumList[1];
+            int z = NumList[2];
+
+            foreach(int i in NumList)
+            {
+                Console.WriteLine(i);
+            }
+
+            NumList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        { 
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
 
     }
